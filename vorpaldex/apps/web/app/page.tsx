2cca50e11942348d42be4571bf9870fe09dba119
Yuriy <berlandspw@gmail.com>
2023-10-28 16:50:39 +0500

    Turbo package installed

---
+++ vorpaldex/apps/web/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Card } from "../../../packages/ui";
+import styles from "./page.module.css";
+
+export default function Page(): JSX.Element {
+  return (
+    <main className={styles.main}>
+          <h1>
+            Vorpal DEX
+          </h1>
+    </main>
+  );
+}
